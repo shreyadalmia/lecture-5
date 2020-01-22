7f5e5dd6887caa1e416db17c39d91657d08aad05
shreyadalmia <59814211+shreyadalmia@users.noreply.github.com>
2020-01-22 08:39:48 +0530

    Add files via upload

---
+++ practice codes/q1.py
@@ -0,0 +1,9 @@
+dict = {}
+dict['one'] = "this is one"
+dict[2] = "this is two"
+tinydict = {'name': 'john','code':6734,'dept': 'sales'}
+print(dict['one'])
+print(dict[2])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
